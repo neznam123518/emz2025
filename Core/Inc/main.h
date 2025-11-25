5c2f1b16bda038f49ebd5731cee09ab2e656f7f4
moneygungunov-source <novaccount2003@abv.bg>
2025-11-25 17:30:39 +0200

    first project

---
+++ Core/Inc/main.h
@@ -0,0 +1,138 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_HRTIM_MspPostInit(HRTIM_HandleTypeDef *hhrtim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define JOYSTICK_SEL_Pin GPIO_PIN_13
+#define JOYSTICK_SEL_GPIO_Port GPIOC
+#define JOYSTICK_SEL_EXTI_IRQn EXTI15_10_IRQn
+#define BUCKBOOST_LOAD_1_Pin GPIO_PIN_14
+#define BUCKBOOST_LOAD_1_GPIO_Port GPIOC
+#define BUCKBOOST_LOAD_2_Pin GPIO_PIN_15
+#define BUCKBOOST_LOAD_2_GPIO_Port GPIOC
+#define USBPD_VIN_Pin GPIO_PIN_2
+#define USBPD_VIN_GPIO_Port GPIOC
+#define BUCKBOOST_USBPD_EN_Pin GPIO_PIN_3
+#define BUCKBOOST_USBPD_EN_GPIO_Port GPIOC
+#define BUCK_GREEN_SENSE_Pin GPIO_PIN_0
+#define BUCK_GREEN_SENSE_GPIO_Port GPIOA
+#define BUCKBOOST_VIN_Pin GPIO_PIN_1
+#define BUCKBOOST_VIN_GPIO_Port GPIOA
+#define BUCKBOOST_I_IN_AVG_Pin GPIO_PIN_2
+#define BUCKBOOST_I_IN_AVG_GPIO_Port GPIOA
+#define BUCKBOOST_VOUT_Pin GPIO_PIN_3
+#define BUCKBOOST_VOUT_GPIO_Port GPIOA
+#define BUCK_RED_SENSE_Pin GPIO_PIN_7
+#define BUCK_RED_SENSE_GPIO_Port GPIOA
+#define JOYSTICK_LEFT_Pin GPIO_PIN_4
+#define JOYSTICK_LEFT_GPIO_Port GPIOC
+#define JOYSTICK_LEFT_EXTI_IRQn EXTI4_IRQn
+#define JOYSTICK_DOWN_Pin GPIO_PIN_5
+#define JOYSTICK_DOWN_GPIO_Port GPIOC
+#define JOYSTICK_DOWN_EXTI_IRQn EXTI9_5_IRQn
+#define BUCK_BLUE_SENSE_Pin GPIO_PIN_0
+#define BUCK_BLUE_SENSE_GPIO_Port GPIOB
+#define LD3_Pin GPIO_PIN_1
+#define LD3_GPIO_Port GPIOB
+#define JOYSTICK_RIGHT_Pin GPIO_PIN_2
+#define JOYSTICK_RIGHT_GPIO_Port GPIOB
+#define JOYSTICK_RIGHT_EXTI_IRQn EXTI2_IRQn
+#define JOYSTICK_UP_Pin GPIO_PIN_10
+#define JOYSTICK_UP_GPIO_Port GPIOB
+#define JOYSTICK_UP_EXTI_IRQn EXTI15_10_IRQn
+#define BUCKBOOST_I_IN_SENSE_Pin GPIO_PIN_11
+#define BUCKBOOST_I_IN_SENSE_GPIO_Port GPIOB
+#define BUCKBOOST_P1_DRIVE_Pin GPIO_PIN_12
+#define BUCKBOOST_P1_DRIVE_GPIO_Port GPIOB
+#define BUCKBOOST_N1_DRIVE_Pin GPIO_PIN_13
+#define BUCKBOOST_N1_DRIVE_GPIO_Port GPIOB
+#define BUCKBOOST_N2_DRIVE_Pin GPIO_PIN_14
+#define BUCKBOOST_N2_DRIVE_GPIO_Port GPIOB
+#define BUCKBOOST_P2_DRIVE_Pin GPIO_PIN_15
+#define BUCKBOOST_P2_DRIVE_GPIO_Port GPIOB
+#define BUCK_RED_DRIVE_Pin GPIO_PIN_6
+#define BUCK_RED_DRIVE_GPIO_Port GPIOC
+#define RC_TP4_Pin GPIO_PIN_7
+#define RC_TP4_GPIO_Port GPIOC
+#define BUCK_GREEN_DRIVE_Pin GPIO_PIN_8
+#define BUCK_GREEN_DRIVE_GPIO_Port GPIOC
+#define BUCK_BLUE_DRIVE_Pin GPIO_PIN_8
+#define BUCK_BLUE_DRIVE_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_15
+#define LD2_GPIO_Port GPIOA
+#define USART3_TX_Pin GPIO_PIN_10
+#define USART3_TX_GPIO_Port GPIOC
+#define USART3_RX_Pin GPIO_PIN_11
+#define USART3_RX_GPIO_Port GPIOC
+#define USBPD_1A_PROTECT_Pin GPIO_PIN_12
+#define USBPD_1A_PROTECT_GPIO_Port GPIOC
+#define USBPD_550mA_PROTECT_Pin GPIO_PIN_2
+#define USBPD_550mA_PROTECT_GPIO_Port GPIOD
+#define LD5_Pin GPIO_PIN_5
+#define LD5_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/stm32g4xx_it.c
@@ -0,0 +1,309 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32g4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32g4xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+extern ADC_HandleTypeDef hadc1;
+extern HRTIM_HandleTypeDef hhrtim1;
+extern UART_HandleTypeDef huart3;
+extern PCD_HandleTypeDef hpcd_USB_FS;
+extern TIM_HandleTypeDef htim1;
+
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+   while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Prefetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32G4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32g4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles EXTI line2 interrupt.
+  */
+void EXTI2_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI2_IRQn 0 */
+
+  /* USER CODE END EXTI2_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(JOYSTICK_RIGHT_Pin);
+  /* USER CODE BEGIN EXTI2_IRQn 1 */
+
+  /* USER CODE END EXTI2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line4 interrupt.
+  */
+void EXTI4_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI4_IRQn 0 */
+
+  /* USER CODE END EXTI4_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(JOYSTICK_LEFT_Pin);
+  /* USER CODE BEGIN EXTI4_IRQn 1 */
+
+  /* USER CODE END EXTI4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles ADC1 and ADC2 global interrupt.
+  */
+void ADC1_2_IRQHandler(void)
+{
+  /* USER CODE BEGIN ADC1_2_IRQn 0 */
+
+  /* USER CODE END ADC1_2_IRQn 0 */
+  HAL_ADC_IRQHandler(&hadc1);
+  /* USER CODE BEGIN ADC1_2_IRQn 1 */
+
+  /* USER CODE END ADC1_2_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USB high priority interrupt remap.
+  */
+void USB_HP_IRQHandler(void)
+{
+  /* USER CODE BEGIN USB_HP_IRQn 0 */
+
+  /* USER CODE END USB_HP_IRQn 0 */
+  HAL_PCD_IRQHandler(&hpcd_USB_FS);
+  /* USER CODE BEGIN USB_HP_IRQn 1 */
+
+  /* USER CODE END USB_HP_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[9:5] interrupts.
+  */
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(JOYSTICK_DOWN_Pin);
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+
+  /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM1 update interrupt and TIM16 global interrupt.
+  */
+void TIM1_UP_TIM16_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_UP_TIM16_IRQn 0 */
+
+  /* USER CODE END TIM1_UP_TIM16_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  /* USER CODE BEGIN TIM1_UP_TIM16_IRQn 1 */
+
+  /* USER CODE END TIM1_UP_TIM16_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART3 global interrupt / USART3 wake-up interrupt through EXTI line 28.
+  */
+void USART3_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART3_IRQn 0 */
+
+  /* USER CODE END USART3_IRQn 0 */
+  HAL_UART_IRQHandler(&huart3);
+  /* USER CODE BEGIN USART3_IRQn 1 */
+
+  /* USER CODE END USART3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[15:10] interrupts.
+  */
+void EXTI15_10_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI15_10_IRQn 0 */
+
+  /* USER CODE END EXTI15_10_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(JOYSTICK_UP_Pin);
+  HAL_GPIO_EXTI_IRQHandler(JOYSTICK_SEL_Pin);
+  /* USER CODE BEGIN EXTI15_10_IRQn 1 */
+
+  /* USER CODE END EXTI15_10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles HRTIM master timer global interrupt.
+  */
+void HRTIM1_Master_IRQHandler(void)
+{
+  /* USER CODE BEGIN HRTIM1_Master_IRQn 0 */
+
+  /* USER CODE END HRTIM1_Master_IRQn 0 */
+  HAL_HRTIM_IRQHandler(&hhrtim1,HRTIM_TIMERINDEX_MASTER);
+  /* USER CODE BEGIN HRTIM1_Master_IRQn 1 */
+
+  /* USER CODE END HRTIM1_Master_IRQn 1 */
+}
+
+/**
+  * @brief This function handles HRTIM timer F global interrupt.
+  */
+void HRTIM1_TIMF_IRQHandler(void)
+{
+  /* USER CODE BEGIN HRTIM1_TIMF_IRQn 0 */
+
+  /* USER CODE END HRTIM1_TIMF_IRQn 0 */
+  HAL_HRTIM_IRQHandler(&hhrtim1,HRTIM_TIMERINDEX_TIMER_F);
+  /* USER CODE BEGIN HRTIM1_TIMF_IRQn 1 */
+
+  /* USER CODE END HRTIM1_TIMF_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
